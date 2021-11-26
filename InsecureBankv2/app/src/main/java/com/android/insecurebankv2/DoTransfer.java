a1b4ae675d336762dbbc07cbd0f7a63ccacd5203
ɪʟᴋᴋᴀ ᴛᴜʀᴜɴᴇɴ <1454138+Iletee@users.noreply.github.com>
2021-11-26 17:24:32 +0000

    Update DoTransfer.java

--- InsecureBankv2/app/src/main/java/com/android/insecurebankv2/DoTransfer.java
+++ InsecureBankv2/app/src/main/java/com/android/insecurebankv2/DoTransfer.java
@@ -120,7 +120,24 @@ public class DoTransfer extends Activity {
 		public void AsyncHttpTransferPost(String string) {
 			//do something
 		}
-
+		
+		public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Order rhs = (Order) obj;
+        return new EqualsBuilder()
+                .append(this.id, rhs.id)
+                .append(this.clientName, rhs.clientName)
+                .append(this.amount, rhs.amount)
+                .isEquals();
+    }
 		/**
 		 * background functions
 		 */
